b0463d526d0e37e81bb1f99076c1f5be35fcddb8
boarboar123 <alexander.ershov@gmail.com>
2023-09-14 11:39:23 +0300

    direction icons, text field to nav, test impl

--- app/src/main/java/com/example/myapplication/NavActivity.kt
+++ app/src/main/java/com/example/myapplication/NavActivity.kt
@@ -1,25 +1,32 @@
 package com.example.myapplication
 
-import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
-import android.widget.Toast
-import androidx.activity.ComponentActivity
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
 
 
 class NavActivity : AppCompatActivity() {
+    private val icons = arrayOf(R.drawable.baseline_arrow_upward, R.drawable.baseline_arrow_right,
+        R.drawable.baseline_arrow_downward, R.drawable.baseline_arrow_left, )
+    private val testText = arrayOf("10", "100", "500", "1000")
+    private lateinit var drawables: ArrayList<Drawable>
+    private var dir = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layoutInflater.inflate(R.layout.activity_nav, null))
+        drawables = icons.map { AppCompatResources.getDrawable(applicationContext, it) } as ArrayList<Drawable>
     }
 
     fun onTurn(view: View) {
-        Toast.makeText(applicationContext, "Turn", Toast.LENGTH_SHORT).show()
-        val drawable = AppCompatResources.getDrawable(applicationContext, R.drawable.baseline_arrow_right)
-        findViewById<ImageView>(R.id.imageView).setImageDrawable(drawable)
+        //Toast.makeText(applicationContext, "Turn", Toast.LENGTH_SHORT).show()
+        dir = (dir + 1) % 4
+        findViewById<ImageView>(R.id.imageView).setImageDrawable(drawables[dir])
+        findViewById<TextView>(R.id.textView).text = testText[dir]
     }
 }
 
